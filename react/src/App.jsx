22e46cefc51f6a77a7a0a4c09a8cadceae73619d
kaushani rasadika <rasadikak@gmail.com>
2026-03-25 22:14:37 +0530

    created component files

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,18 @@
+from "./components/Contact" import Contact
+
+
+
 function App(){
   return(
     <>
-    
+    <head>
+      <title>Kaushani Rasadika</title>
+    </head>
     <h2> Im Kaushani Rasadika</h2>
+    <h4>From Sri Lanka</h4>
+    <h3>AI enthuassiastic | Full Stack Developer</h3>
+
+    </Contact>
     </>
     
   );
