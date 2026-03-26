2c7830fb4395ba56ff9d0f5af959e39fe7aa1cea
kaushani rasadika <rasadikak@gmail.com>
2026-03-26 12:25:11 +0530

    created  hero section

--- react/src/components/Hero.jsx
+++ react/src/components/Hero.jsx
@@ -2,7 +2,21 @@ import {styles} from './Hero.module.css'
 function Hero() {
   return (
     <>
-      <div>
+      <div className={styles.hero}>
+
+        <div className={styles.myName}>
+        <h2>Kaushani Rasadika</h2>
+        </div>
+
+        <div className={styles.job}>
+        <h3>IT Undergraduate</h3>
+        </div>
+
+        <div className={styles.description}>
+        <h4>A passionate software engineer building modern web applications
+           with React, Angular, Java and Spring Boot. Explore my projects, 
+           articles, and professional journey.</h4>
+        </div>
       
     </div>
     </>
